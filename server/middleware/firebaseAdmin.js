e7267a759711a5fc7a26126bf0d5b71c226837dd
Frédéric Beuserie <fbeuserie@gmail.com>
2025-05-16 10:18:10 +0900

    chore: moved firebase config from .json file to .env variable

--- server/middleware/firebaseAdmin.js
+++ server/middleware/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 
-const serviceAccount = require('../secure/firebase-server-sa.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVER_CONFIG);
 
 const app = initializeApp({
   credential: cert(serviceAccount)
